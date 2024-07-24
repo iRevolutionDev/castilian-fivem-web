374201074e38a913257ae19a7b713ec527c5ba1e
Revolution <tavinho.daniel@gmail.com>
2024-07-24 12:47:25 -0300

    feat: add window dragging system and animation

--- src/app/[locale]/(application)/cheat/layout.tsx
+++ src/app/[locale]/(application)/cheat/layout.tsx
@@ -1,19 +1,44 @@
 "use client";
 import { Navbar } from "@/components/drawer";
+import { useWindowSize } from "@/hooks/use-window-size";
 import { Box } from "@mui/material";
-import type { ReactNode } from "react";
+import { motion } from "framer-motion";
+import { type ReactNode, useRef } from "react";
 
 export default function CheatWindow({
 	children,
 }: Readonly<{ children: ReactNode }>) {
+	const containerRef = useRef<HTMLDivElement>(null);
+	const { width, height } = useWindowSize();
+
 	return (
-		<div className="h-screen">
-			<div
-				style={{
+		<div className="h-screen overflow-hidden" ref={containerRef}>
+			<Box
+				component={motion.div}
+				sx={{
 					width: 1280,
 					height: 720,
+					backgroundColor: (theme) => theme.palette.background.paper,
+					position: "absolute",
+					boxShadow: 24,
 				}}
-				className="bg-neutral-950 text-white absolute inset-0 m-auto overflow-hidden rounded-3xl"
+				style={{
+					top: (height - 720) / 2,
+					left: (width - 1280) / 2,
+				}}
+				className="text-white overflow-hidden rounded-3xl"
+				drag
+				dragConstraints={containerRef}
+				dragMomentum={false}
+				initial={{
+					opacity: 0,
+				}}
+				animate={{
+					opacity: 1,
+				}}
+				exit={{
+					opacity: 0,
+				}}
 			>
 				<div className="relative h-full w-full flex-col">
 					<Navbar>
@@ -22,7 +47,7 @@ export default function CheatWindow({
 						</Box>
 					</Navbar>
 				</div>
-			</div>
+			</Box>
 		</div>
 	);
 }
